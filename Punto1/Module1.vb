b4986026898256fed4ac93b9d32bc1446843d7e0
JoanSebastianingsof <jsgonzalezorozco@gmail.com>
2022-08-17 10:22:53 -0500

    Cambios minimos

--- Punto1/Module1.vb
+++ Punto1/Module1.vb
@@ -3,8 +3,7 @@
     Sub Main()
         'Realizar un programa que genere una matriz 3x3, inicializarla en 0 e imprimirla.
         Dim num As Integer = 0
-        Dim filas As Integer
-        Dim columnas As Integer
+
         Dim matriz(3, 3) As Double
         For i = 0 To matriz.GetUpperBound(0) - 1
             For j = 0 To matriz.GetUpperBound(1) - 1
--- Punto2/Module1.vb
+++ Punto2/Module1.vb
@@ -4,8 +4,7 @@
         'Realizar un programa que genere una matriz 6x6, inicializarla la diagonal principal en 0, el resto de posiciones deben ser *. Imprimir la matriz.
         Dim num As Char = ""
 
-        Dim filas As Integer
-        Dim columnas As Integer
+
         Dim matriz(6, 6) As Char
         For i = 0 To matriz.GetUpperBound(0) - 1
             For j = 0 To matriz.GetUpperBound(1) - 1
